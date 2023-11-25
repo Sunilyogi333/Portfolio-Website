14ccfd0d02a42fa4d0380d7e6a77d36f479ea8c3
Sunilyogi333 <sunil.211443@ncit.edu.np>
2023-11-25 21:21:25 +0545

    some changes

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("This is my first code in javascript");
+console.log('Hello world');
+const header = document.querySelector('.header');
+const mobileNav = document.querySelector('.mobile-nav-toggle');
+
+mobileNav.addEventListener('click', () => {
+    header.classList.toggle('active');
+    });
+
+
+// active link with scroll
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        if(window.scrollY >= (section.offsetTop) - section.clientHeight / 3) {
+            current = section.id;
+            console.log(current);
+        }
+    });
+    navLinks.forEach(navLink => {
+        navLink.classList.remove('opened');
+        if(navLink.href.includes(current)) {
+            navLink.classList.add('opened');
+        }
+    });
+});
