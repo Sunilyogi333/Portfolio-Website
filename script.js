03f919fc66d197ce8c7f8748f254b6040115966c
Sunilyogi333 <sunil.211443@ncit.edu.np>
2023-12-03 20:04:20 +0545

    some changes

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-console.log('Hello world');
+// Header
+
 const header = document.querySelector('.header');
 const mobileNav = document.querySelector('.mobile-nav-toggle');
+const navLinks = document.querySelectorAll('.nav-link');
 
+// navbar show/hide on click
 mobileNav.addEventListener('click', () => {
     header.classList.toggle('active');
     });
 
+//Closing sidebar when clicking on a link
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        header.classList.remove('active');
+    });
+}
+); 
 
-// active link with scroll
-const navLinks = document.querySelectorAll('.nav-link');
+// Closing sidebar when clicking outside of it
+window.addEventListener('click', (e) => {
+    if (!header.contains(e.target) && !e.target.classList.contains('mobile-nav-toggle')) {
+        header.classList.remove('active');
+    }
+});
+
+// active link on scroll
 const sections = document.querySelectorAll('section');
-
 window.addEventListener('scroll', () => {
     let current = '';
     sections.forEach(section => {
